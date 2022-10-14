3289fccdbf57d76e3cf086c1e8f92d012275cb9f
A Park <atpark333@gmail.com>
2022-10-14 14:22:31 +0900

    Add implementation for BFT

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 import networkx as nx
 from typing import NamedTuple
 from queues import Queue
+from collections import deque
 
 class City(NamedTuple):
     name: str
@@ -48,3 +49,40 @@ def breadth_first_search(graph, source, predicate, order_by=None):
     for node in breadth_first_traverse(graph, source, order_by):
         if (predicate(node)):
             return node
+
+def retrace(previous, source, destination):
+    path = deque()
+    current = destination
+    while current != source:
+        path.appendleft(current)
+        current = previous.get(current)
+        if current is None:
+            return None
+
+    path.appendleft(source)
+    return list(path)
+
+def shortest_path(graph, source, destination, order_by=None):
+    queue = Queue(source)
+    visited = {source}
+    previous = {}
+    while queue:
+        node = queue.dequeue()
+        neighbors = list(graph.neighbors(node))
+        if order_by:
+            neighbors.sort(key=order_by)
+        for neighbor in neighbors:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.enqueue(neighbor)
+                previous[neighbor] = node
+                if neighbor == destination:
+                    return retrace(previous, source, destination)
+
+def print_shortest_path(graph, city1, city2):
+    return " --> ".join(city.name for city in shortest_path(graph, city1, city2))
+
+
+def connected(graph, source, destination):
+    return shortest_path(graph, source, destination) is not None
+
